c189cd2c4174b0c833f93cdaf41fba004f386aa0
Shane Winslow <winslow.shane2@gmail.com>
2026-01-22 21:35:20 +0000

    fixing dbt setup

--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -8,8 +8,7 @@ customers as (
 
     select
         id as customer_id,
-        first_name,
-        last_name
+        name
 
     from source
 
--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,7 +1,7 @@
 select
     id as order_id,
-    user_id as customer_id,
-    order_date,
-    status
+    customer as customer_id,
+    ordered_at as order_date,
+    order_total as amount
 
 from {{source('jaffle_shop', 'orders')}}
